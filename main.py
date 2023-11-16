12cacb2647ed8f0785fe13bf4c2a22d23ad021ab
Dimitris Anagnostopoulos <d.anagnostopoulos02@gmail.com>
2023-11-16 14:49:39 +0200

    .

--- main.py
+++ main.py
@@ -4,35 +4,30 @@ from Models import scikitlearn_lr as sl_lr
 
 
 def main():
-    RMSE_values = []
+    test_RMSE_values = []
+    sl_RMSE_values = []
 
     for _ in range(20):
-        RMSE = my_lr.test_lr()
-        RMSE_values.append(RMSE)
+        test_RMSE = my_lr.test_lr()
+        test_RMSE_values.append(test_RMSE)
 
-    mean_RMSE = np.mean(RMSE_values)
-    std_RMSE = np.std(RMSE_values)
+        sl_RMSE = sl_lr.scikitlearn_lr()
+        sl_RMSE_values.append(sl_RMSE)
+
+    test_mean_RMSE = np.mean(test_RMSE_values)
+    test_std_RMSE = np.std(test_RMSE_values)
+
+    sl_mean_RMSE = np.mean(sl_RMSE_values)
+    sl_std_RMSE = np.std(sl_RMSE_values)
 
     print("==========={My Linear Regression Model}===========")
-    print(f"Mean RMSE: {mean_RMSE}")
-    print(f"Standard Deviation of RMSE: {std_RMSE}\n")
-
-
-def scikitlearn_LR():
-    RMSE_values = []
-
-    for _ in range(20):
-        RMSE = sl_lr.scikitlearn_lr()
-        RMSE_values.append(RMSE)
-
-    mean_RMSE = np.mean(RMSE_values)
-    std_RMSE = np.std(RMSE_values)
+    print(f"Mean RMSE: {test_mean_RMSE}")
+    print(f"Standard Deviation of RMSE: {test_std_RMSE}\n")
 
     print("==========={SciKit Learn Linear Regression Model}===========")
-    print(f"Mean RMSE: {mean_RMSE}")
-    print(f"Standard Deviation of RMSE: {std_RMSE}\n")
+    print(f"Mean RMSE: {sl_mean_RMSE}")
+    print(f"Standard Deviation of RMSE: {sl_std_RMSE}\n")
 
 
 if __name__ == "__main__":
     main()
-    scikitlearn_LR()
