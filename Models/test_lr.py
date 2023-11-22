d81e512063256bd530db5c636990dbe920892eff
Dimitris Anagnostopoulos <jaygap24@gmail.com>
2023-11-22 13:10:30 +0200

    .

--- Models/test_lr.py
+++ Models/test_lr.py
@@ -1,25 +1,14 @@
 import numpy as np
-from sklearn.datasets import fetch_california_housing
-from sklearn.model_selection import train_test_split
 from Classes.linear_regression import LinearRegression
 
 
-def test_lr():
-    df = fetch_california_housing(as_frame=True)
-
-    X = df.data
-    y = df.target
-
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+def test_lr(sets):
+    X_train, X_test, y_train, y_test = sets
 
     model = LinearRegression()
 
-    X_train = np.array(X_train)
-    y_train = np.array(y_train)
     model.fit(X_train, y_train)
 
-    X_test = np.array(X_test)
-    y_test = np.array(y_test)
     y_hat, MSE = model.evaluate(X_test, y_test)
 
     RMSE = np.sqrt(MSE)
--- main.py
+++ main.py
@@ -1,17 +1,32 @@
 import numpy as np
 from Models import test_lr as my_lr
 from Models import scikitlearn_lr as sl_lr
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
+
+
+def train_Test_Sets():
+    data = fetch_california_housing(as_frame=True)
+
+    X = data.data
+    y = data.target
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+    return [np.array(X_train), np.array(X_test), np.array(y_train), np.array(y_test)]
 
 
 def main():
     test_RMSE_values = []
     sl_RMSE_values = []
 
+    sets = train_Test_Sets()
+
     for _ in range(20):
-        test_RMSE = my_lr.test_lr()
+        test_RMSE = my_lr.test_lr(sets)
         test_RMSE_values.append(test_RMSE)
 
-        sl_RMSE = sl_lr.scikitlearn_lr()
+        sl_RMSE = sl_lr.scikitlearn_lr(sets)
         sl_RMSE_values.append(sl_RMSE)
 
     test_mean_RMSE = np.mean(test_RMSE_values)
